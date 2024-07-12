26b695f3ef3ca8e2f6bcf33dde324f2b034e4a56
trgcghai <conghai.tpma@gmail.com>
2024-07-12 09:00:19 +0700

    prepare

---
+++ app/_component/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <h1>hello world from header</h1>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'main': '#c0ff6b',
+        'submain': '#656565',
+        'main-fade': '#d5d5d5'
+      }
     },
   },
   plugins: [],
